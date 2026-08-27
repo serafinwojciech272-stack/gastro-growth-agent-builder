d80a3aa56cb90bacb19bce4760ae85c661a5df3c
serafinwojciech272-stack <serafinwojciech272@gmail.com>
2026-08-27 13:36:28 +0200

    feat(saas): add router and auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
-import { LanguageProvider } from './context/LanguageContext.tsx';
+import { AuthProvider } from './context/AuthContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
